5e53c7b94a55121688f2054306eb34dd93c5e798
camponogaraviera <vieracamponogara@gmail.coma>
2025-08-13 06:06:37 +0800

    init

---
+++ src/helper_functions/decoding.py
@@ -0,0 +1,20 @@
+from typing import List  # For typing annotation.
+from qiskit.circuit import QuantumCircuit # Importing QuantumCircuit for circuit operations.
+import torch  # Importing torch for tensor operations.
+
+def decode_actions_into_circuit(actions: List) -> QuantumCircuit:
+    """ 
+    Decode the actions taken by the agent into quantum circuit operations.
+
+    Args:
+        actions (List): A list of actions taken by the agent, where each action corresponds to a gate operation.
+
+    Returns:
+        qc (QuantumCircuit): A quantum circuit constructed based on the agent's actions.
+    """
+
+    '''
+    Write your code here.
+    '''
+
+    pass
---
+++ src/helper_functions/encoding.py
@@ -0,0 +1,20 @@
+from typing import List  # For typing annotation.
+from qiskit.circuit import QuantumCircuit
+import torch  # Importing torch for tensor operations.
+
+def encode_circuit_into_input_embedding(qc: QuantumCircuit) -> torch.Tensor:
+    """
+    Encode (convert) the quantum circuit into a tensor representation for input to the agent.
+
+    Args:
+        qc (QuantumCircuit): The quantum circuit to be encoded.
+
+    Returns:
+        torch.Tensor: The tensor representation of the quantum circuit.
+    """
+
+    '''
+    Write your code here.
+    '''
+
+    pass
